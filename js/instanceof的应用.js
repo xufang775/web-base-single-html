4f489378c696cf2616c031c7a7e6807ea541d177
xufang775 <xufang461010923@163.com>
2020-03-26 12:32:12 +0800

    instanceof的应用

---
+++ js/instanceof的应用.js
@@ -0,0 +1,12 @@
+/**
+ * instanceof是用来判断A是否是B的实例
+ */
+
+ console.log([] instanceof Array);
+ console.log({} instanceof Object);
+ console.log(new Date() instanceof Date);
+ console.log(new Date() instanceof Object);
+ console.log(null instanceof null);
+ console.log(undefined instanceof undefined);
+ 
+ 
